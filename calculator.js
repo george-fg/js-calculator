5266ffbb88fe5b6fdd8cf0ef4d66cb09513ecbde
AhmedShab <ahmed.vuw@gmail.com>
2016-07-07 13:28:20 +1200

    added events for the buttons along with displaying operators and numbers on the input

--- calculator.js
+++ calculator.js
@@ -1,7 +1,44 @@
 $(document).ready(function() {
 
-$(".row1").children().click(function(event) {
-  console.log("im working!");
-});
+  $(".row1").children().click(runFun);
+  $(".row2").children().click(runFun);
+  $(".row3").children().click(runFun);
+  $(".row4").children().click(runFun);
 
 });
+
+function runFun(event) {
+  var target = event.target.innerHTML;
+  var numbers = getNumbers(target);
+
+
+  switch (target) {
+    case "AC":
+      return $("#displayText").val("0");
+    case "CE":
+
+    case "&":
+
+    case "/":
+    return calculate("/");
+    case "*":
+    return calculate("*");
+    case "+":
+    return calculate("+");
+    case "-":
+    return calculate("-");
+  }
+
+  if (!isNaN(numbers)) {
+    $("#displayText").val(numbers);
+    calculate(numbers);
+  }
+}
+
+function calculate(op, num) {
+  console.log(op, num);
+}
+
+function getNumbers(target) {
+   return parseInt(target);
+}
